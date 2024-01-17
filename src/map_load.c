2cba4cd988e1a9b7fae920411c110f5a4da6b0ce
alex <alejandrocp_952@hotmail.com>
2024-01-17 18:07:03 +0100

    v2_bonus

--- src/map_load.c
+++ src/map_load.c
@@ -6,40 +6,11 @@
 /*   By: alcarden <alcarden@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 19:13:54 by alcarden          #+#    #+#             */
-/*   Updated: 2024/01/15 17:11:31 by alcarden         ###   ########.fr       */
+/*   Updated: 2024/01/17 18:06:01 by alcarden         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
-
-// void	ft_read_map(char *file, t_element *element)
-// {
-// 	int		fd;
-// 	char	*temp;
-// 	char	*big_str;
-// 	char	*str;
-
-// 	fd = open(file, O_RDONLY);
-// 	big_str = malloc(sizeof(char) * 2);
-// 	if (fd == -1)
-// 		perror("File error or empty");
-// 	str = get_next_line(fd);
-// 	if (!str)
-// 		perror("Empty file");
-// 	while (str != NULL && str[0] != '\0')
-// 	{
-// 		temp = ft_strjoin(big_str, str);
-// 		free(big_str);
-// 		big_str = temp;
-// 		free(str);
-// 		str = get_next_line(fd);
-// 	}
-// 	free(str);
-// 	free(temp);
-// 	close(fd);
-// 	element->full_map = ft_split(big_str, '\n');
-// 	element->map_cpy = ft_split(big_str, '\n');
-// }
 
 char	*ft_read_map(char *file)
 {
@@ -119,62 +90,20 @@ t_element	*ft_gen_map(mlx_t *mlx, t_element *element)
 		}
 		i++;
 	}
-	//element = ft_gen_player(mlx, element);
 	return (element);
 }
 
-// t_element	*ft_gen_player(mlx_t *mlx, t_element *element)
-// {
-// 	int	i;
-// 	int	j;
-
-// 	i = 0;
-// 	while (element->full_map[i])
-// 	{
-// 		j = 0;
-// 		while (element->full_map[i][j])
-// 		{
-// 			if (element->full_map[i][j] == 'P')
-// 			{
-// 				ft_valere(mlx, i, j, element);
-// 				element->player_pos_x = j;
-// 				element->player_pos_y = i;
-// 			}
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// 	return (element);
-// }
-
-// t_element	ft_gen_map_enemy(t_element element)
-// {
-// 	int	i;
-// 	int	j;
-
-// 	i = 0;
-// 	while (element.full_map[i])
-// 	{
-// 		j = 0;
-// 		while (element.full_map[i][j])
-// 		{
-// 			if (element.full_map[i][j] == '1')
-// 				mlx_image_to_window(element.mlx, element.wall, j * 64, i * 64);
-// 			else if (element.full_map[i][j] == '0')
-// 				mlx_image_to_window(element.mlx, element.floor, j * 64, i * 64);
-// 			else if (element.full_map[i][j] == 'C')
-// 				ft_collectible(i, j, element);
-// 			else if (element.full_map[i][j] == 'E')
-// 				ft_exit(i, j, element);
-// 			else if (element.full_map[i][j] == 'B')
-// 				ft_enemy(i, j, element);
-// 			j++;
-// 		}
-// 		i++;
-// 	}
-// 	{
-// 		element = ft_gen_player(element);
-// 		return (element);
-// 	}
-// }
+void ft_valere_idle(mlx_t *mlx, t_element *element)
+{
+	if (element->is_idle == 0)
+	{
+		element->is_idle = 1;
+		element->idle_timer = 0;
+	}
+	else
+	{
+		element->is_idle = 0;
+		element->idle_timer = 0;
+	}
+}
 
